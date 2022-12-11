a4209e509ef0f9055ea74f5b9d8f0885fc9ad2ef
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-12-11 23:16:17 +0530

    Add files via upload

---
+++ del_nodes_greater_value_on_right.cpp
@@ -0,0 +1,40 @@
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+     Node *compute(Node *head)
+     {
+          // your code goes here
+          Node *curr = head;
+          while (curr->next)
+          {
+               if (curr->data < curr->next->data)
+               {
+                    curr->data = curr->next->data;
+                    Node *temp = curr->next;
+                    curr->next = curr->next->next;
+                    delete temp;
+                    curr = head;
+               }
+               else
+                    curr = curr->next;
+          }
+          return head;
+     }
+};
+
+
