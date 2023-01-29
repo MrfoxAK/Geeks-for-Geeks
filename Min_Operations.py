e6a6a434c20a27bac5f4450860b7e8dc899bbb12
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-01-29 10:46:29 +0530

    Add files via upload

---
+++ Min_Operations.py
@@ -0,0 +1,24 @@
+a = 12
+b = 5
+
+def solve(self, a : int, b : int) -> int:
+
+        if a==b:return 0
+
+        if ((a&b)==a or (a&b)==b):return 1
+
+        return 2
+
+
+
+
+
+print(a & b)
+
+
+
+
+
+
+
+
