d2f4a9936b5327394a4a3acea9cd0a137ef68501
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2024-03-24 07:45:33 +0530

    Add files via upload

---
+++ insert_bottom_of_stack.cpp
@@ -0,0 +1,21 @@
+//User function Template for C++
+
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st, int x) {
+        stack<int> temp;
+        while(!st.empty()) {
+            temp.push(st.top());
+            st.pop();
+        }
+        
+        temp.push(x);
+        
+        while(!temp.empty()) {
+            st.push(temp.top());
+            temp.pop();
+        }
+        
+        return st;
+    }
+};
