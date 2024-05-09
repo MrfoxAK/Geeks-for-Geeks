38baa4d43a2835e34214ac57268afb147651fa36
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2024-05-09 20:09:30 +0530

    Add files via upload

---
+++ Divisor_Game.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+     bool divisorGame(int n)
+     {
+          // code here
+          if (n % 2 == 0)
+          {
+               return true;
+          }
+          else
+          {
+               return false;
+          }
+     }
+};
