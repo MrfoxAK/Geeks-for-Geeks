6c6e8749cdfba44ab65506964808eb7eeb48463e
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2024-09-09 12:29:34 +0530

    Add files via upload

---
+++ Minimum_Jumps.cpp
@@ -0,0 +1,30 @@
+// Function to return minimum number of jumps to end of array
+
+class Solution
+{
+public:
+     int minJumps(vector<int> &arr)
+     {
+          // Your code here
+          int jump = 0;
+          int n = arr.size();
+          int maxi = 0;
+          int mrange = 0;
+
+          for (int i = 0; i < n; i++)
+          {
+               maxi = max(maxi, arr[i] + i);
+               if (mrange == i)
+               {
+                    mrange = maxi;
+                    jump++;
+                    if (mrange >= n - 1)
+                    {
+                         return jump;
+                    }
+               }
+          }
+
+          return -1;
+     }
+};
